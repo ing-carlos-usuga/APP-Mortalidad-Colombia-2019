98c06d865cb55c036dc6bbacf13739ed78e7e1f5
Carlos Usuga <carlosusuga@MacBook-Air-de-Carlos.local>
2025-11-02 13:03:30 -0500

    Configuración estable para Render con Python 3.10

--- app.py
+++ app.py
@@ -226,4 +226,6 @@ app.layout = html.Div([
 # EJECUCIÓN
 
 if __name__ == "__main__":
-    app.run(debug=False, port=8060)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host="0.0.0.0", port=port, debug=False)
+
