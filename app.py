26ef6fe4366895915168941bc4223ce3951bc842
Carlos Usuga <carlosusuga@MacBook-Air-de-Carlos.local>
2025-11-02 14:26:46 -0500

    Configuración estable para Render con Python 3.10

--- app.py
+++ app.py
@@ -164,6 +164,7 @@ def histograma_por_grupo_edad(df):
 
 
 app = Dash(__name__)
+lanzamiento=app.run
 app.title = "Mortalidad no fetal en Colombia - 2019"
 df = cargar_datos()
 div = cargar_divipola()
