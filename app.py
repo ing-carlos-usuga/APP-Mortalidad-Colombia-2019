136e745b93c524f0b0d97b383dd2628e2bef146a
Carlos Usuga <carlosusuga@MacBook-Air-de-Carlos.local>
2025-11-02 15:38:43 -0500

    Actualización del código en VS Code

--- app.py
+++ app.py
@@ -226,6 +226,6 @@ app.layout = html.Div([
 # EJECUCIÓN
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8050))
     app.run(host="0.0.0.0", port=port, debug=False)
 
